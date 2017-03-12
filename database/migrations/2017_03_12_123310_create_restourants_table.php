49983750bb9e36d8aa1ee30ef158ff4ffe7a5f1f
mashrur786 <mashru_uk@hotmail.com>
2017-03-12 15:22:30 +0000

    restaurants table created via migration

--- database/migrations/2017_03_12_123310_create_restourants_table.php
+++ database/migrations/2017_03_12_123310_create_restourants_table.php
@@ -15,6 +15,20 @@ class CreateRestourantsTable extends Migration
     {
         Schema::create('restaurants', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('email')->unique();
+            $table->string('Business_name');
+            $table->text('description')->nullable();
+            $table->string('cuisine', 20);
+            $table->string('Business_phone1');
+            $table->string('Business_phone2')->nullable();
+            $table->string('address');
+            $table->string('street');
+            $table->string('town');
+            $table->string('county');
+            $table->string('postcode' , 10);
+            $table->text('website')->nullable();
+            $table->string('contact_name');
+            $table->string('contact_phone');
             $table->timestamps();
         });
     }
